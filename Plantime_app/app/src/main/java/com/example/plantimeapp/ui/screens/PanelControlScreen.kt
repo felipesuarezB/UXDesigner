8e0e6e92a3bc4b754ba9e9ac282d75d8a168fe4a
Santiago Gómez Perdomo <s.gomezp2345@uniandes.edu.co>
2025-09-25 07:48:30 -0500

    improving design 4

--- Plantime_app/app/src/main/java/com/example/plantimeapp/ui/screens/PanelControlScreen.kt
+++ Plantime_app/app/src/main/java/com/example/plantimeapp/ui/screens/PanelControlScreen.kt
@@ -3,9 +3,11 @@ package com.example.plantimeapp.ui.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
 
 @Composable
 fun PanelControlScreen(
