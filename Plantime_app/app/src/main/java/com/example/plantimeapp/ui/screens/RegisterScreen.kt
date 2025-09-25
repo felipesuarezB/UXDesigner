df522c7412b205cd997d3e6e45d39b19b8c34e69
Santiago Gómez Perdomo <s.gomezp2345@uniandes.edu.co>
2025-09-25 08:22:46 -0500

    improving design 7

--- Plantime_app/app/src/main/java/com/example/plantimeapp/ui/screens/RegisterScreen.kt
+++ Plantime_app/app/src/main/java/com/example/plantimeapp/ui/screens/RegisterScreen.kt
@@ -1,13 +1,16 @@
 package com.example.plantimeapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
 import com.example.plantimeapp.ui.theme.GreenDark
 import com.example.plantimeapp.ui.theme.GreenPrimary
 
